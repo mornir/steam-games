2863e525d091fe73a2df2cc17fc29ac9fe4b1885
mornir <jeromepott@hotmail.com>
2021-06-10 20:29:08 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="bg-gray-900">
+    <div class="flex flex-wrap gap-8">
+      <GameCard v-for="game in games" :key="game.appid" :game="game" />
+    </div>
+  </div>
+</template>
+
+<script>
+import games from "./games.json"
+import GameCard from "./components/GameCard.vue"
+
+export default {
+  components: {
+    GameCard,
+  },
+  data() {
+    return {
+      games: games,
+    }
+  },
+}
+</script>
---
+++ src/components/GameCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <img
+      :src="`https://steamcdn-a.akamaihd.net/steam/apps/${game.appid}/library_600x900.jpg`"
+      width="150"
+      height="225"
+      loading="async"
+      :alt="game.name"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    game: {
+      type: Object,
+      default: () => {},
+      required: true,
+    },
+  },
+}
+</script>
